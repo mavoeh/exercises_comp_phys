74776c90c48d2a4b47fe8ec9cb07ddbc2d480e81
HelenMey <s6hemey2@uni-bonn.de>
2020-11-17 08:48:20 +0100

    einige kleine änderungen

--- problem2/ising2d.py
+++ problem2/ising2d.py
@@ -4,15 +4,16 @@ import scipy.special
 Jc = 0.440686793509772
 
 
-def H(J, h, config):
+def H(J, h, Nx, Ny, config):
         # calculates Hamiltonian for a Nx*Ny spin configuration
 
         #interaction term with four neighbours (in 2 dimensions)
-        interact = np.sum(config[:-1,:-1]*config[:-1,1:]*config[1:,:-1])
-        interact += np.sum(config[0]*config[-1]*np.roll(config[-1], -1)) + np.sum(np.transpose(config)[-1]*np.transpose(config)[0]*np.roll(np.transpose(config)[-1], -1))
-        interact -= config[-1,-1]*config[-1,0]*config[0,-1]
+        interact = np.sum(config[1:,:]*config[:-1,:])+np.sum(config[:, 1:]*config[:, :-1])
+        if Nx > 2:
+            interact += np.sum(config[0,:] *config[-1,:])
+        if Ny > 2:
+            interact += np.sum(config[:,0] *config[:,-1])
         interact *= -J
-
         #coupling to external magnetic field
         external = -h*np.sum(config)
         return interact + external
@@ -23,17 +24,15 @@ def sweep(config, Nx, Ny, J, h):
     Implements Metropolis-Hastings method for a spin configuration config in a lattice with Nx*Ny spins
 
     Input:
-    random spin configuration   config
-    number of spins in x-dir    Nx
+    random spin configuration   config    number of spins in x-dir    Nx
     number of spins in y-dir    Ny
     coupling constant           J
     magnetic coupling           h
 
     Output:
-    configuration with smallest possible energy    config
+    configuration with smallest possible energy    listconfigs
 
     '''
-    H0 = H(J, h, config)
 
     #sweep through the lattice
     for i in range(Nx):
@@ -43,23 +42,17 @@ def sweep(config, Nx, Ny, J, h):
             config[i,j] *= -1
 
             #calculate change in energy
-            Hflip = H(J, h, config)
-            delta = Hflip - H0
+            delta = -2*config[i,j]*(h + J*(config[(i-1)%Nx, j] + config[i, (j-1)%Ny] + config[i, (j+1)%Ny] + config[(i+1)%Nx,j]))
 
-            #metropolis-hastings step (how to choose step?)
-            y = np.random.choice(np.arange(0, 1, 0.0001))
+            #metropolis-hastings step
+            y = np.random.uniform(0,1)
 
-            if(y < np.exp(-delta)):
-                H0 = Hflip
-                #print("flip:", y, np.exp(-delta))
-            else:
+            if(y > np.exp(-delta)):
                 config[i,j] *= -1
-                #print("noflip:", y, np.exp(-delta))
+
     return config
 
-
-
-def simulate(J, h, Nx, Ny, n, Nmeas):
+def simulate(J, h, Nx, Ny, Nmeas):
     
     '''
     Implements Metropolis-Hastings method for a spin configuration config in a lattice with Nx*Ny spins
@@ -73,73 +66,44 @@ def simulate(J, h, Nx, Ny, n, Nmeas):
     number of measurements      Nmeas
 
     Output:
+    J, h, Nx, Ny
+    average multiplicaton       m
+    statistical error           delm
+    average energy              eps
+    statistical error           deleps
+    exact value for |m|         absm_exact
+    exact value for eps         eps_exact        
     
     '''
 
-    # initialize and assign the boltzmann factors of the configs to z_array
-    def zarray(J, h, n, listconfigs):
-        z_array = np.zeros(n)
-        for i in range(len(z_array)):
-            z_array[i] = np.exp(-H(J,h, listconfigs[i]))
-        return z_array
-    
-    # calculate the partition function
-
-    def Z(z_array):
-        return np.sum(z_array)
-
     mray = np.zeros(Nmeas)
-    Zray = np.zeros(Nmeas)
     epsray = np.zeros(Nmeas)
     
 
     for t in range(Nmeas):
 
-        # initialize list of 2d array for n random spin configurations
-        listconfigs = []
+        # sample spin configuration 
+    
+        config = np.random.choice([-1, 1], (Nx, Ny)) 
 
-        for i in range(n):
-                config = np.zeros((Nx, Ny))
+        config = sweep(config, Nx, Ny, J, h)
 
-                for j in range(Nx):
-                    config[j] = np.random.choice([-1, 1], Ny) 
-
-                listconfigs.append(config)
-
-        #for each spin configuration in listconfigs do a sweep through the lattice
-
-        for config in listconfigs:
-            sweep(config, Nx, Ny, J, h)
-
-        
-        #calculate m for each spin configuration (here: by using formula (4) on the exercise sheet)
-        m_array = np.zeros(n)
-        for i in range(len(m_array)):
-            m_array[i] = np.sum(listconfigs[i])*np.exp(-H(J, h, listconfigs[i]))
+        #calculate m for each spin configuration 
+        mray[t] = 1./Nx/Ny*np.sum(config)
 
         #calculate energy per site
-        eps_array = np.zero(n)
-        for i in range(n):
-            eps_array[i] = H(J, h, lisconfigs[i]) 
+        epsray[t] = 1/Nx/Ny*np.sum(H(J, h, Nx, Ny, config))
 
 
-        #sum over all configs to get estimate for m, eps and Z
-        epsray[t] = 1/Nx/Ny*np.sum(eps_array)
-        Zray[t] = Z(zarray(J, h, n, listconfigs))
-        mray[t] = 1./N/Zray[t]*np.sum(m_array)
-
-
-    #Calculate mean value of m and Z (average over all "measurements")
-
-    Zres = np.mean(Zray)
+    #Calculate mean value of m and eps (average over all "measurements")
     m = np.mean(mray)
+    absm = np.mean(np.abs(mray))
     eps = np.mean(epsray)
 
-    #Calculate statistical error of m and Z and relative deviation to analytical result. Display results if show_results = True
-    delZres = np.std(Zray)
+    #Calculate statistical error of m and Z
     delm = np.std(mray)
-    deleps = np.mean(epsray)
-
+    delabsm = np.std(np.abs(mray))
+    deleps = np.std(epsray)
 
     absm_exact = 0
     eps_exact = 0
@@ -150,15 +114,14 @@ def simulate(J, h, Nx, Ny, n, Nmeas):
 
         eps_exact = -J/np.tanh(2*J)*(1 + 2./np.pi*(2*np.tanh(2*J)**2 - 1)*scipy.special.ellipk(4/np.cosh(2*J)**2*np.tanh(2*J)**2))
 
-    return h, J, Nx, Ny, m, delm, eps, deleps, absm_exact, eps_exact
+    return h, J, Nx, Ny, m, delm, eps, deleps, absm, delabsm, absm_exact, eps_exact
 
 
-n = 10
 Nmeas = 100
 N_array = np.arange(4, 21, 1)
 
 #calculate m as a function of h for fixed J < 1, Nx = Ny between 4 and 20
-# loop ove rall combinations
+# loop over all combinations
 h_array = np.linspace(-1, 1, 21) # h in steps of 0.1
 J = 0.5
 
@@ -168,11 +131,11 @@ for i in range(len(N_array)):
         print("N = {0:d} from 20, h = {1:5.2f} from -1 to 1 in steps of 0.1".format(N,h), end = "\r")
         # write output of the simulate function to a text file
         f = open("results_m.txt", "a")
-        f.write("{0:.2f}\t{1:.2f}\t{2:d}\t{3:d}\t{4:e}\t{5:e}\t{6:e}\t{7:e}\t{8:e}\t{9:e}\n".format(*simulate(J, h, N, N, n, Nmeas)))
+        f.write("{0:.2f}\t{1:.2f}\t{2:d}\t{3:d}\t{4:e}\t{5:e}\t{6:e}\t{7:e}\t{8:e}\t{9:e}\t{10:e}\t{11:e}\n".format(*simulate(J, h, N, N, Nmeas)))
         f.close()
 
 # calculate absm and eps for h=0 as a function of J in [.25, 1] and [.25, 2]
-# for absm take abs value of m when plotting :)
+# for absm take abs value of m when plotting
 
 J_array = np.linspace(0.25, 2, 100)
 h = 0
@@ -183,9 +146,6 @@ for i in range(len(N_array)):
         print("N = {0:d} from 20, J = {1:5.2f} from 0.25 to 2 in steps of ?".format(N,J), end = "\r")
         # write output of the simulate function to a text file
         f = open("results_absm_eps.txt", "a")
-        f.write("{0:.2f}\t{1:.2f}\t{2:d}\t{3:d}\t{4:e}\t{5:e}\t{6:e}\t{7:e}\t{8:e}\t{9:e}\n".format(*simulate(J, h, N, N, n, Nmeas)))
+        f.write("{0:.2f}\t{1:.2f}\t{2:d}\t{3:d}\t{4:e}\t{5:e}\t{6:e}\t{7:e}\t{8:e}\t{9:e}\t{10:e}\t{11:e}\n".format(*simulate(J, h, N, N, Nmeas)))
         f.close()
-
-
-
 
