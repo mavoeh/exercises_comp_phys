29207438cdf59a3f194ec8617aceb122209b951f
Marc Vöhringer Carrera <marc.voeh@gmail.com>
2020-11-23 12:04:43 +0100

    changed deviation to absolute value

--- problem3/leapfrog.py
+++ problem3/leapfrog.py
@@ -20,22 +20,23 @@ def leapfrog(func, p, phi, Nmd):
     return p, phi
 
 Nmd_max = 100
-p0 = 100
-phi0 = 100
+p0 = 1
+phi0 = 1
 H0 = H(p0, phi0)
 x = range(1,Nmd_max+1)
 y = np.zeros(Nmd_max)
 for Nmd in x:
-    y[Nmd-1] = H(*leapfrog(H,p0,phi0,Nmd)) - H0
-y /= H0
+    y[Nmd-1] = np.abs(H(*leapfrog(H,p0,phi0,Nmd)) - H0)
+y /= np.abs(H0)
 
+print(y)
 plt.scatter(x,y)
 plt.xlim(0,Nmd+1)
 plt.yscale("log")
-plt.ylim(1e-3,1)
+plt.ylim(2e-4,1e1)
 plt.grid(True)
 plt.xlabel(r"$N_\mathrm{md}$",fontsize=18)
-plt.ylabel(r"$\frac{H(p_f,\phi_f)-H(p_0,\phi_0)}{H(p_0,\phi_0)}$",fontsize=20)
+plt.ylabel(r"$\frac{|H(p_f,\phi_f)-H(p_0,\phi_0)|}{|H(p_0,\phi_0)|}$",fontsize=20)
 
 plt.show()
     
