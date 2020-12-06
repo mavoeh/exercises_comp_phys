8b32d1bc613c03c8a2d6f2b24fe1ccce1b98ce46
HelenMey <s6hemey2@uni-bonn.de>
2020-12-06 21:17:22 +0100

    fixed error in markov chain

--- problem5/multigrid.py
+++ problem5/multigrid.py
@@ -12,10 +12,10 @@ def H(u, a):
 
 
 @jit(nopython=True)
-def metropolis_hastings_step(H, u, delta, a):
+def metropolis_hastings_step(u, delta, a):
     # perform the hmc metropolis hastings step
     r = np.random.uniform(-1,1)
-    x = np.random.choice(np.arange(1, N-1, 1)) #u(0) = u(N)= 0
+    x = np.random.choice(np.arange(1, N-1, 1)) #boundary conditions u(0) = u(N)= 0
     change = delta*r 
     DeltaH = 2./a*(u[x]**2 - (u[x]+change)**2 + change*u[x-1] + change*u[x+1])
     if np.random.uniform(0,1) <= np.exp(DeltaH):
@@ -26,13 +26,13 @@ def metropolis_hastings_step(H, u, delta, a):
         return False, u # if rejected return False and old u
 
 @jit(nopython=True)
-def sweep(H, u, pars):
+def sweep(u, pars):
     N, beta, a, delta = pars
     counter = 0
     for i in range(N-1):
-        c, u = metropolis_hastings_step(H, u, delta, a)
+        c, u = metropolis_hastings_step(u, delta, a)
         if c:
-            counter += 1
+            counter += 1.
     return counter, u
 
 
@@ -56,15 +56,20 @@ def generate_markov_chain(Ncfg, Ntherm, pars, u0):
     
     # thermalize
     for i in range(Ntherm):
-        _, u0 = sweep(H, u0, pars)
-    
-    # initialize array for phi values
+        _, u0 = sweep(u0, pars)
+
+    # initialize list for u arrays and arrays to store measurement for m and energy
     u_list = []
+    #m_array = np.zeros(Ncfg)
+    #eps_array = np.zeros(Ncfg)
     counter = 0 # counter for acceptance rate
     for i in range(Ncfg):
-        accepted, u = sweep(H, u0, pars)
-        u_list.append(u)
-        u0 = u_list[i]
+        accepted, u = sweep(u0, pars)
+        u_list.append(list(u))
+        u0 = np.array(u_list[i])
+        #m_array[i] = np.array(u_list[i]).mean()
+        #eps_array[i] = H(np.array(u_list[i]), a)
+
         counter += accepted
     
     counter /= Ncfg*(N-1) # normalize counter
@@ -81,29 +86,28 @@ a = 1.
 delta = 2.
 pars = (N, beta, a, delta)
 
-Ncfg = 100
-Ntherm = 200
+Ncfg = 10000
+Ntherm = 1000000
 
-u0 = np.zeros(N) 
+u0 = np.zeros(N)
 
-counter, u_list = generate_markov_chain(Ncfg, Ntherm, pars, u0)
-
+counter, u_list  = generate_markov_chain(Ncfg, Ntherm, pars, u0)
 
 
 #calculate mean value for m and energy
-
 m_array = np.zeros(Ncfg)
 eps_array = np.zeros(Ncfg)
 
-# plot history of m and eps
+#plot history of m and eps
 for i in range(Ncfg):
-    m_array[i] = 1./N*np.sum(u_list[i])
-    eps_array[i] = H(u_list[i], a)
+    m_array[i] = np.array(u_list[i]).mean()
+    eps_array[i] = H(np.array(u_list[i]), a)
+
 
 fig_mhistory = plt.figure()
 ax = plt.gca()
 
-ax.plot(np.arange(0, Ncfg, 1), eps_array,
+ax.plot(np.arange(0, Ncfg, 1), m_array,
             linestyle = "none",
             marker = "o",
             markersize = 4,
@@ -116,8 +120,7 @@ fig_mhistory.tight_layout()
 
 fig_mhistory.savefig("m_history.pdf")
 
-m = 1./Ncfg*np.sum(m_array)
-eps = 1./Ncfg*np.sum(eps_array)
-
+m = m_array.mean()
+eps = eps_array.mean()
 
 print(m, eps)
