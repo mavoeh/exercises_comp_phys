ee1587249cabf37ab52449465b78e1404e59239c
HelenMey <s6hemey2@uni-bonn.de>
2021-03-06 14:52:47 +0100

    scattering lengths

--- project/2plus1scat.py
+++ project/2plus1scat.py
@@ -7,28 +7,50 @@ from fsi import *
 
 m = 938.92 #MeV
 
+Lam = [700.0]
+e0list = [-2.125, -2.225, -2.325]
+Energies = np.array([0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 1, 2, 4])
+names = ["txtfiles/totEe125.txt", "txtfiles/totEe225.txt", "txtfiles/totEe325.txt"]
 
+for j, e0 in enumerate(e0list):
+	stot = []
+	for i, Elab in enumerate(Energies):
+		para = fitc0(Lam,e0list[1])[0]
+		pot=OBEpot(nx=24,mpi=138.0,C0=para[1],A=-1.0/6.474860194946856,cutoff=para[0])
+
+		ed, _, _, _ = TwoBody(pot, np1=20, np2=10).esearch()
+
+		scattL0 = ThreeBodyScatt(pot,e3n=(2./3.*Elab/ThreeBodyScatt.hbarc + ed),nx=16,np1=20,np2=10,nq1=15,nq2=5,lmax=0,bl=0)
+		ed *= scattL0.hbarc
+
+		#get elastic cross section
+		theta = np.arccos(scattL0.xp)
+		melast = scattL0.get_m_elast_drive(theta) + scattL0.get_m_elast_pw(theta)
+		sigma = (2*np.pi)**4*2./3.*(m/scattL0.hbarc)**2*np.absolute(melast)**2
+		
+		#integrate to get total cross section
+		stot.append(2.*np.pi*np.sum(scattL0.xw*sigma))
+
+
+
+plt.plot(Energies, stot)
+plt.show()
+
+
+'''
 para2=[700.0, 0.020167185806378923]
 pot=OBEpot(nx=24,mpi=138.0,C0=para2[1],A=-1.0/6.474860194946856,cutoff=para2[0])
 
 ed, _, _, _ = TwoBody(pot, np1=20, np2=10).esearch()
 
-theta1 = 39.
-theta2 = 39.
-phi12 = 180.
-Elab = 13.
+theta1 = 10.0
+theta2 = 45.0
+phi12 = 45.
+Elab = 25.
 
 scattL0 = ThreeBodyScatt(pot,e3n=(2./3.*Elab/ThreeBodyScatt.hbarc + ed),nx=16,np1=20,np2=10,nq1=15,nq2=5,lmax=0,bl=0)
 ed *= scattL0.hbarc
 
-theta = np.array([theta1])
-
-melast = scattL0.get_m_elast_drive(theta) + scattL0.get_m_elast_pw(theta)
-
-sigma = (2*np.pi)**4*2./3.*(m/scattL0.hbarc)**2*np.absolute(melast)**2/scattL0.hbarc*10.
-print(sigma)
-
-'''
 S, _, kx, ky, t = scurve(theta1, theta2, phi12, Elab, e = ed, deg=True)
 Nc = 1000
 Ns = 10**5+1
@@ -36,8 +58,8 @@ S = S[::int(np.round(Ns/Nc))]
 kx = kx[::int(np.round(Ns/Nc))]
 ky = ky[::int(np.round(Ns/Nc))]
 
-f = open("phi180th39E13.txt", "w")
-f.write("phi12 = 180°, theta1 = theta2 = 39°, Elab = 13 MeV. Compare to Glöckle S.189\n")
+f = open("phi45th1045E25.txt", "w")
+f.write("phi12 = 45°, theta1 = 10°, theta2 = 45.0°, Elab = 25 MeV \n")
 sigma = np.zeros(len(S))
 for i in range(len(S)):
 	sigma[i] = scattL0.breakup_cross(Elab, kx[i], ky[i], theta1, theta2 ,phi12)
